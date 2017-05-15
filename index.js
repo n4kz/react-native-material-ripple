3858628688d700e00bca04af04b386ee5a91f448
n4kz <n4kz@n4kz.com>
2017-05-15 10:13:40 +0300

    Use PropTypes from prop-types package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
 import { View, Animated, Easing, TouchableWithoutFeedback } from 'react-native';
 import { styles, radius } from './styles.js';
 
